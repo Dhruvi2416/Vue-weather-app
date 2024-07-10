da9786e67c1f2e70c9a7b2bc95faecd580f37c28
Dhruvi Shah <dhruvi.shah@motadata.com>
2024-07-10 15:53:52 +0530

    Created a page with countries options

---
+++ src/components/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="sticky top-0 shadow-lg bg-weather-primary">
+    <nav
+      class="container py-6 flex flex-col sm:flex-row text-white justify-center gap-4"
+    >
+      <RounterLink>
+        <div class="gap-3 items-center justify-center flex">
+          <i class="fa-solid fa-cloud-sun text-2xl"></i>
+          <div class="text-2xl">Weather Prediction App</div>
+          <i class="fa-solid fa-cloud-sun text-2xl"></i>
+        </div>
+      </RounterLink>
+    </nav>
+  </header>
+</template>
+
+<script></script>
---
+++ src/views/HomeComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <!-- <input
+      id="search-city"
+      class="p-2 rounded-md focus:outline-none focus:border-white shadow-2xl"
+    /> -->
+    <select
+      v-model="selectedCountry"
+      id="country"
+      name="country"
+      class="form-control px-3 py-2"
+    >
+      <option value="" selected disabled>Select a country</option>
+      <option
+        v-for="(country, index) in countries"
+        :key="index"
+        :value="country"
+      >
+        {{ country }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+import countries from "../constants/countries";
+export default {
+  name: "HomeComponent",
+  data() {
+    return {
+      countries,
+      selectedCountry: "",
+    };
+  },
+  mounted() {
+    document.getElementById("search-city").focus();
+  },
+};
+</script>
